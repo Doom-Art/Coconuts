414a92de9db344254274021e1b7fafa51dbfdb71
Doom-Art <ouginak121@gmail.com>
2022-12-01 11:57:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Coconuts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string i in File.ReadLines(@"coconut_test.txt")){
+                int c = 0;
+                int x;
+                int p;
+                bool success = true;
+                while (success)
+                {
+                    c++;
+                    Int32.TryParse(i, out x);
+                    p = c;
+                    for (int j = 0; j < 5; j++)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
